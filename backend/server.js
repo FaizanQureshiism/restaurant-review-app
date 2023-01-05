108154bfe61864c050247b8477f1c17ba2774729
Farhan <farhanqureshiism@gmail.com>
2023-01-05 19:53:39 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from "express"
+import cors from "cors"
+import restaurants from "./api/restaurants.route.js"
+
+const app = express()
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+//Route 
+app.use("/api/v1/restaurnats", restaurants)
+app.use("*", (req, res) => res.status(404).json({error:"not found"}))
+
+export default app
