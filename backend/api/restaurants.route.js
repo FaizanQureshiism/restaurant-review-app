20399a70f956c8463a4b3b9f376ecd4de68dbfa8
FaizanQureshiism <faizanqureshiism@gmail.com>
2023-01-08 17:53:23 +0530

    DOA changes

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -1,7 +1,8 @@
-import express from "express"
-import RestaurantsCtrl from "./restaurants.controller.js"
+import express from "express";
+import RestaurantsCtrl from "./restaurants.controller.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.route("/").get(RestaurantsCtrl.apiGetRestaurnats)
-export default router
+router.route("/").get(RestaurantsCtrl.apiGetRestaurnats);
+
+export default router;
--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,23 @@
-import app from "./server.js"
-import mongodb from "mongodb"
-import dotenv from "dotenv"
-import RestaurantsDAO from "./dao/restaurantsDOA.js"
-dotenv.config()
+import app from "./server.js";
+import mongodb from "mongodb";
+import dotenv from "dotenv";
+import RestaurantsDAO from "./dao/restaurantsDAO.js";
+dotenv.config();
 
-const MongoClient = mongodb.MongoClient
-const port = process.env.port || 8000
+const MongoClient = mongodb.MongoClient;
+const port = process.env.port || 8000;
 
-MongoClient.connect(
-    process.env.RESTREVIEWS_DB_URI,
-    {
-        maxPoolSize:50,
-        wtimeoutMS:2500, }
-    )
-    .catch(err => {
-        console.error(err.stack)
-        process.exit(1)
-    })
-    .then(async client => {
-        await RestaurantsDAO.injectDB(client)
-        app.listen(port, () => {
-            console.log(`listening on port ${port}`)
-        })
-    })
+MongoClient.connect(process.env.RESTREVIEWS_DB_URI, {
+  maxPoolSize: 50,
+  wtimeoutMS: 2500,
+})
+  .catch((err) => {
+    console.error(err.stack);
+    process.exit(1);
+  })
+  .then(async (client) => {
+    await RestaurantsDAO.injectDB(client);
+    app.listen(port, () => {
+      console.log(`listening on port ${port}`);
+    });
+  });
